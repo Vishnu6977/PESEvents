62a059c7cafeefcb8874a0993aa4707e5118380a
Yathartha19 <aarushyathartha19@gmail.com>
2024-10-14 19:47:29 +0530

    final home commit?

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -52,19 +52,18 @@ const Home = () => {
               Timeline
               <span className='block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-current'></span>
             </div>
-            <button className='bg-white text-black rounded-2xl px-5 py-3 shadow-lg hover:bg-black hover:text-white transition duration-300'>
+            <button className='navbtn rounded-2xl px-5 py-3 shadow-lg'>
               Get Started
             </button>
           </div>
         </nav>
-
         
 
         <div className='flex flex-row items-center low:justify-center lg:justify-between mx-20 low:pt-[35vh] lg:pt-[30vh]'>
           <div className='text-white font-thin lg:text-lg xl:text-xl max-w-[70vh]'>
             <img src='logo.png' className='lg:h-32 xl:h-40 mb-16' alt='logo' />
             <p className='low:hidden lg:flex text-left'>
-              Whether you're organizing a PES event or attending one, our platform guarantees a lowooth, efficient experience. Finding and registering for events has never been easier. We're here to simplify event management for everyone.
+              Whether you're organizing a PES event or attending one, our platform guarantees a smooth, efficient experience. Finding and registering for events has never been easier. We're here to simplify event management for everyone.
             </p>
             <p className=' lg:hidden text-center'>
               Events of PES
--- frontend/src/components/HomeUnder.jsx
+++ frontend/src/components/HomeUnder.jsx
@@ -36,7 +36,17 @@ const HomeUnder = () => {
       </div>
 
 
-      <div className='flex flex-col items-center low:pt-[10vh] lg:pt-[19vh] xl:pt-[17vh] pb-24'>
+      <div className='flex flex-col items-center justify-center orgteambg2 h-[55vh] mt-24 p-4 text-center'>
+          <h1 className='font-lale text-white low:text-4xl lg:text-6xl'>Interested in Clubs/Events?</h1>
+          <p className='text-white text-xl font-thin pt-6'>Get to know about any Event/Club now!</p>
+          <div className='flex flex-row gap-10'>
+            <button className='bg-white rounded-xl py-4 px-8 text-black font-sans mt-12 shadow-2xl hover:bg-black hover:text-white'>Events</button>
+            <button className='bg-white rounded-xl py-4 px-8 text-black font-sans mt-12 shadow-2xl hover:bg-black hover:text-white'>Clubs</button>
+          </div>
+      </div>
+
+
+      <div className='flex flex-col items-center low:pt-[8vh] lg:pt-[17vh] xl:pt-[15vh] pb-24'>
         <h1 className='font-thin low:text-4xl lg:text-6xl pb-12'>What We Offer</h1>  
         <div className='flex low:flex-col lg:flex-row items-start justify-between gap-4 font-thin low:text-xl lg:text-2xl low:mx-4 lg:mx-0'>
           <div className='max-w-[70vh]'>
@@ -65,15 +75,15 @@ const HomeUnder = () => {
       </div>
 
       
-      <div className='flex flex-col items-center justify-center orgteambg h-[60vh] mb-24 p-4 text-center'>
+      <div className='flex flex-col items-center justify-center orgteambg h-[55vh] mb-24 p-4 text-center'>
           <h1 className='font-lale text-white low:text-4xl lg:text-6xl'>Part of an Organising Team?</h1>
           <p className='text-white text-xl font-thin pt-6'>Get your Event/Club onto our site today!</p>
-          <button className='btn-clr-2 rounded-xl py-4 px-6 text-white font-sans mt-12 shadow-2xl hover:border'>Get Started</button>
+          <button className='bg-white rounded-xl py-4 px-6 text-black font-sans mt-12 shadow-2xl hover:bg-black hover:text-white'>Get Started</button>
       </div>
 
         
       <div className='flex flex-col items-center pb-24'>
-        <h1 className='font-marv text-6xl pb-20 '>Gallery</h1>
+        <h1 className='font-thin text-6xl pb-20 '>Gallery</h1>
       </div>
 
       <Footer />
